b1ce0b52d8e8449aa65a9077e2289050e1b5e908
Simone Marcato <simone@hevelop.com>
2023-09-02 21:12:14 +0200

    configured basic routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
 
-function App() {
+import { Route, Router, Routes } from "react-router-dom";
+import { useLayoutEffect, useState } from "react";
+import { createBrowserHistory } from "history";
+// create an App component with a react-router-dom Switch
+
+export default function App() {
+  const history = createBrowserHistory();
+
+  const [state, setState] = useState({
+    action: history.action,
+    location: history.location,
+  });
+  useLayoutEffect(() => history.listen(setState), [history]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router
+        location={state.location}
+        navigationType={state.action}
+        navigator={history}
+      >
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route default element={() => <h1>404</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
-export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+// create a component called Home
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
